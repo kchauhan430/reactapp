5d6984844de18e803c380af02472ed6972e8ffb7
Kanchan Chauhan <kchauhan430@gmail.com>
2024-01-18 12:27:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './HeaderComponent.js';
 import Footer from './FooterComponent.js';
+import MainContent from './MainContent.js';
 
 
 function App() {
   return (
     <div className="App">
           <Header />
+          <MainContent />
           <Footer />
     </div>
   );
