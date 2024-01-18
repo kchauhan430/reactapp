06469f41083deac6ef8a58e4d6feabd7a174a79a
Kanchan Chauhan <kchauhan430@gmail.com>
2024-01-18 12:28:57 +0530

    changes

---
+++ src/MainContent.js
@@ -0,0 +1,9 @@
+function maincontent(){
+    return(
+        <div className="main-content">
+            <h1>Welcome to my first ReactJS App</h1>
+        </div>
+    );
+}
+
+export default maincontent;
